2978f5191a96a4b0eab0274769c3912879c0e504
bpmaurya <realvedprakash@gmail.com>
2021-08-15 21:49:35 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const data = ["Rahul", "Aniket", "Robert", "Poem", "Kumar"];
+
+  const [number, setNumber] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
+  const reset = () => {
+    setNumber(0);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    let interval: any = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setNumber(number + 1);
+      }, 1000);
+    } else if (!isActive && number !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, number]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello </h1>
+      <div>{data[number]}</div>
+      <div>
+        <button
+          className={`button button-primary-${
+            isActive ? "active" : "inactive"
+          }`}
+          onClick={toggle}>
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
